5d6c0d73e9fe4810561167c25c7bd30f0edd37d9
Apsu <aapeli.haataja@gmail.com>
2025-06-17 13:06:22 +0300

    Add local files to github

---
+++ script.js
@@ -0,0 +1,32 @@
+function goTo(page) {
+  window.location.href = page;
+}
+
+/*Learning Hub*/
+
+
+// Placeholder function for A1 button
+function actionA1() {
+  console.log("A1 clicked");
+  // Add functionality here
+}
+
+// Placeholder function for GO button
+function actionGo() {
+  var text = document.getElementById("inputText").value;
+  var vocab = document.getElementById("vocabText").value;
+  
+  // Add functionality here
+}
+
+// Placeholder function for CLEAR button
+function actionClear() {
+  console.log("Clear clicked");
+  // Add functionality here
+}
+
+// Placeholder function for HOW TO? button
+function actionHowTo() {
+  console.log("How To clicked");
+  // Add functionality here
+}
